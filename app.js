cf480b7231deb7b7039f992fdc85628fd18e6572
Krystian <vexiseffects@gmail.com>
2019-10-20 16:46:25 +0200

    set routes, update templates, update images

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ app.set('view engine', 'pug')
 app.use('/static', express.static('public'))
 
 app.get('/', (req, res) => {
-    res.render('index')
+    console.log(data.projects[0])
+
+    res.render('index', { object: data.projects })
 })
 
 app.get('/about', (req, res) => {
@@ -17,7 +19,21 @@ app.get('/about', (req, res) => {
 app.get('/project/:id', (req, res) => {
     const projectId = req.params.id
     console.log(projectId)
-    res.render('project')
+    res.render('project', {object: data.projects[projectId]})
+})
+
+app.use((req, res, next) => {
+    const err = new Error('Not found')
+    err.status = 404;
+    next(err)
+})
+
+
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error', err);
+    console.log('Sorry, page not found')
 })
 
 
